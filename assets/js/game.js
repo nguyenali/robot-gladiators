429c283fe757829895ecad21deb1d960f3eb9460
Ali Nguyen <alin57717@gmail.com>
2020-08-04 00:27:04 -0700

    add variable to create condiitions

--- assets/js/game.js
+++ assets/js/game.js
@@ -21,7 +21,15 @@ console.log(
   playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
 );
 
-// Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable
+// check enemy's health
+if (enemyHealth <= 0) {
+  window.alert(enemyName + " has died!");
+} 
+else {
+  window.alert(enemyName + " still has " + enemyHealth + " health left.");
+}
+
+ // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable
 playerHealth = playerHealth - enemyAttack;
 
 // Log a resulting message to the console so we know that it worked.
@@ -29,23 +37,20 @@ console.log(
   enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
 );
 
-// put new code under this
-console.log(
-  playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
-);
-
-// check enemy's health
-if (enemyHealth <= 0) {
-  window.alert(enemyName + " has died!");
-} 
-
 // check player's health
 if (playerHealth <= 0) {
   window.alert(playerName + " has died!");
 } 
-
 else {
-  window.alert(enemyName + " still has " + enemyHealth + " health left.");
+  window.alert(playerName + " still has " + playerHealth + " health left.");
 }
+
+
+
+
+
+
+
+};
 
 fight();
